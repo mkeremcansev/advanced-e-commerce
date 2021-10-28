a1ae984c1388a49dd3f50d65ef354d342a1c2309
mkeremcansev <mkeremcansev@gmail.com>
2021-10-28 04:08:13 +0300

    Mysql utf8_general_ci updated

--- config/database.php
+++ config/database.php
@@ -53,7 +53,7 @@ return [
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8',
-            'collation' => 'utf8_unicode_ci',
+            'collation' => 'utf8_general_ci',
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
